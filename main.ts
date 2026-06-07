f323ebb0a7e998354cfd880880d8f04eee2dec76
calliope-net <141074597+calliope-net@users.noreply.github.com>
2026-06-07 14:29:34 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,12 +4,23 @@ function GitHub () {
 }
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     callibot2.comment("Ultraschall Abstand Sensor")
-    callibot2.write_motor_prozent(callibot2.eMotor.beide, 80)
-    callibot2.wait_us(20)
-    callibot2.write_rgbled(0xa300ff)
+    for (let index = 0; index < 6; index++) {
+        callibot2.write_motor_prozent(callibot2.eMotor.beide, 80)
+        callibot2.write_rgbled(0xffff00)
+        callibot2.wait_us(20)
+        callibot2.write_rgbled(0xa300ff)
+        if (Math.randomBoolean()) {
+            callibot2.write_motoren_prozent(50, -50)
+        } else {
+            callibot2.write_motoren_prozent(-50, 50)
+        }
+        callibot2.comment("drehen in zufällige Richtung, dann weiter fahren")
+        callibot2.wait_motor(callibot2.callibot2_ePause(callibot2.ePause.p1))
+    }
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     callibot2.comment("nur Calli:bot 2E mit Encoder")
+    callibot2.comment("sonst durch 'fahre weiter x Sekunden' ersetzen")
     for (let index = 0; index < 4; index++) {
         callibot2.write_rgbled(0x00ff00)
         callibot2.write_motoren_prozent(60, 60)
@@ -31,8 +42,8 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
         basic.setLedColor(0x00ff00)
     } else {
         callibot2.comment("B: STOP")
-        callibot2.write_motor_prozent(callibot2.eMotor.beide, 0)
         go = false
+        callibot2.write_motor_prozent(callibot2.eMotor.beide, 0)
         basic.setLedColor(0xff0000)
     }
 })
@@ -41,12 +52,13 @@ callibot2.reset_outputs()
 basic.showNumber(callibot2.read_power())
 basic.showString(callibot2.read_typ())
 basic.forever(function () {
-    if (go) {
+    if (go && callibot2.read_compare_us(callibot2.eVergleich.lt, 15)) {
+        callibot2.comment("Ultraschall Hindernis")
+        callibot2.write_motor_prozent(callibot2.eMotor.beide, 0)
+    } else if (go) {
         callibot2.comment("Spur folgen")
         callibot2.read_inputs()
-        if (callibot2.read_compare_us(callibot2.eVergleich.lt, 15)) {
-            callibot2.write_motor_prozent(callibot2.eMotor.beide, 0)
-        } else if (!(callibot2.get_inputs(callibot2.eINPUTS.sp2l)) && !(callibot2.get_inputs(callibot2.eINPUTS.sp1r))) {
+        if (!(callibot2.get_inputs(callibot2.eINPUTS.sp2l)) && !(callibot2.get_inputs(callibot2.eINPUTS.sp1r))) {
             callibot2.comment("beide Sensoren dunkel: auf der Spur")
             callibot2.write_motor_prozent(callibot2.eMotor.beide, 100)
         } else if (callibot2.get_inputs(callibot2.eINPUTS.sp2l) && !(callibot2.get_inputs(callibot2.eINPUTS.sp1r))) {
